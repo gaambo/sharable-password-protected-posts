dec5d0b409c0b746e1fae6a80f68dbfaf99a967e
Fabian Todt <mail@fabiantodt.at>
2022-11-02 10:04:05 +0100

    publish v1.0.0

--- plugin/sharable-password-protected-posts.php
+++ plugin/sharable-password-protected-posts.php
@@ -5,7 +5,7 @@
  * Description:       Share password protected posts via secret URLs
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            Fabian Todt
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
